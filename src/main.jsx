fc3a0326f0ac30fdf12a05ce65ac1040fc13771e
Jyotish Yadav <136741028+Jyotish2002@users.noreply.github.com>
2024-12-03 11:10:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CourseContextProvider } from "./context/CourseContext.jsx";
 
 
 
-export const server = "http://localhost:5000";
+export const server = "https://elearning-server-z1mr.onrender.com";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
